2fd3117b97c64d46fdb4795bbd24e88a25a7e9eb
Rasm221f <rasmusIJ@outlook.dk>
2023-02-20 10:26:44 +0100

    Laver getInt funktion med while loop

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,32 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(getString("Hvad hedder du?"));
-
+        //System.out.println(getString("Hvad hedder du?"));
+        System.out.println(getInt("Angiv venligst din skostørrelse: "));
     }
 
-    public static String getString(String s){
+    public static String getString(String s) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println(s + " :");
         return scanner.nextLine();
     }
+
+    public static int getInt(String s) {
+
+        int res = 0;
+
+
+        while (true) {
+            try {
+                res = Integer.parseInt(getString(s));
+                return res;
+
+            } catch (Exception e) {
+                System.out.println("Husk ikke tal ord");
+            }
+        }
+    }
+
 
 }
